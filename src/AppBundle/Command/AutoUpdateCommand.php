fb17775666eed63d152c29f70f285be3270e9f3b
Tim Davis <binary.god@gmail.com>
2016-01-30 16:51:07 -0500

    - Added PI Material Prices

--- src/AppBundle/Command/AutoUpdateCommand.php
+++ src/AppBundle/Command/AutoUpdateCommand.php
@@ -36,6 +36,15 @@ class AutoUpdateCommand extends ContainerAwareCommand
                         '30370','30378','30371','30372','30373','30374','25273','28697','25277',
                         '28698','25276','28699','25278','28700','25274','28701');
         $mineralOres = array('34','35','36','37','38','39','40','11399');
+        $p0Ores = array('2268','2305','2267','2288','2287','2307','2272','2309','2073','2310',
+                        '2270','2306','2286','2311','2308');
+        $p1Ores = array('2393','2396','3779','2401','2390','2397','2392','3683','2389','2399',
+                        '2395','2398','9828','2400','3645');
+        $p2Ores = array('2329','3828','9836','9832','44','3693','15317','3725','3689','2327',
+                        '9842','2463','2317','2321','3695','9830','3697','9838','2312','3691','2319','9840','3775','2328');
+        $p3Ores = array('2358','2345','2344','2367','17392','2348','9834','2366','2361','17898',
+                        '2360','2354','2352','9846','9848','2351','2349','2346','12836','17136','28974');
+        $p4Ores = array('2867','2868','2869','2870','2871','2872','2875','2876');
 
         // Get Market Helper
         $market = $this->getContainer()->get('market');
@@ -62,5 +71,20 @@ class AutoUpdateCommand extends ContainerAwareCommand
         $logger->info('Auto Updating Minerals');
         $output->writeln('Auto Updating Minerals');
         $market->UpdateCache($mineralOres);
+        $logger->info('Auto Updating P0');
+        $output->writeln('Auto Updating P0');
+        $market->UpdateCache($p0Ores);
+        $logger->info('Auto Updating P1');
+        $output->writeln('Auto Updating P1');
+        $market->UpdateCache($p1Ores);
+        $logger->info('Auto Updating P2');
+        $output->writeln('Auto Updating P2');
+        $market->UpdateCache($p2Ores);
+        $logger->info('Auto Updating P3');
+        $output->writeln('Auto Updating P3');
+        $market->UpdateCache($p3Ores);
+        $logger->info('Auto Updating P4');
+        $output->writeln('Auto Updating P4');
+        $market->UpdateCache($p4Ores);
     }
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -49,11 +49,17 @@ class DefaultController extends Controller
         $iceOres = $this->getQuickReview(array('16264','17975','16265','17976','16262','17978','16263','17977','16267','16268','16266','16269'));
         $gasOres = $this->getQuickReview(array('25268','28694','25279','28695','25275','28696','30375','30376','30377','30370','30378','30371','30372','30373','30374','25273','28697','25277','28698','25276','28699','25278','28700','25274','28701'));
         $mineralOres = $this->getQuickReview(array('34','35','36','37','38','39','40','11399'));
+        $p0Ores = $this->getQuickReview(array('2268','2305','2267','2288','2287','2307','2272','2309','2073','2310','2270','2306','2286','2311','2308'));
+        $p1Ores = $this->getQuickReview(array('2393','2396','3779','2401','2390','2397','2392','3683','2389','2399','2395','2398','9828','2400','3645'));
+        $p2Ores = $this->getQuickReview(array('2329','3828','9836','9832','44','3693','15317','3725','3689','2327','9842','2463','2317','2321','3695','9830','3697','9838','2312','3691','2319','9840','3775','2328'));
+        $p3Ores = $this->getQuickReview(array('2358','2345','2344','2367','17392','2348','9834','2366','2361','17898','2360','2354','2352','9846','9848','2351','2349','2346','12836','17136','28974'));
+        $p4Ores = $this->getQuickReview(array('2867','2868','2869','2870','2871','2872','2875','2876'));
 
         return $this->render('default/index.html.twig', array(
             'base_dir' => 'test', 'page_name' => 'Dashboard', 'sub_text' => 'User Dashboard', 'form' => $form->createView(), 'mode' => 'USER',
          'oSales' => $oSales, 'news' => $news, 'highsecores' => $highSecOres, 'otherhighsecores' => $otherHighSecOres, 'lowsecores' => $lowSecOres,
-         'nullsecores' => $nullSecOres, 'iceores' => $iceOres, 'gasores' => $gasOres, 'mineralores' => $mineralOres));
+         'nullsecores' => $nullSecOres, 'iceores' => $iceOres, 'gasores' => $gasOres, 'mineralores' => $mineralOres, 'p0ores' => $p0Ores,
+         'p1ores' => $p1Ores, 'p2ores' => $p2Ores, 'p3ores' => $p3Ores, 'p4ores' => $p4Ores));
     }
 
     /**
@@ -127,7 +133,7 @@ class DefaultController extends Controller
 		// Loop through items
 		foreach($source as $item) {
 
-            if($item->getTypeId() != '11396' & $item->getTypeId() != '17869' & $item->getTypeId() != '17870' & $item->getTypeId() != '16267') {
+            if($item->getTypeId() != '11396' & $item->getTypeId() != '17869' & $item->getTypeId() != '17870' & $item->getTypeId() != '16267' & $item->getIskPerM() != 0) {
 
     			// Get Valuation of 100 units of ore
     			$value = floor($item->getIskPerM());
@@ -151,7 +157,7 @@ class DefaultController extends Controller
 
 		foreach($source as $item) {
 
-            if($item->getTypeId() != '11396' & $item->getTypeId() != '17869' & $item->getTypeId() != '17870' & $item->getTypeId() != '16267') {
+            if($item->getTypeId() != '11396' & $item->getTypeId() != '17869' & $item->getTypeId() != '17870' & $item->getTypeId() != '16267' & $item->getIskPerM() != 0) {
     			$value = floor($item->getIskPerM());
 
     			// Calculate the percent by getting the spread of the unit
@@ -164,6 +170,8 @@ class DefaultController extends Controller
 
                 $item->setColor('1');
             }
+
+            if($item->getIskPerM() == 0) {$item->setColor('0');}
 		}
 
 		return $source;
