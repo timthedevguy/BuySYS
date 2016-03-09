af0b741241635b4b770a1d2f77e5c3f314cbc93c
Tim Davis <binary.god@gmail.com>
2016-03-08 21:44:12 -0500

    Misc fixes
    - Removed buyback block
    - Updated several dynamic areas

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ amsys
 =====
 
 A Symfony project created on November 19, 2015, 5:11 pm.
+![SeAT](http://i.imgur.com/aPPOxSK.png)
+
+#### A Simple, EVE Online API Tool and Corporation Manager
+
+[![Latest Stable Version](https://poser.pugx.org/eveseat/seat/v/stable)](https://packagist.org/packages/eveseat/seat)
+[![Latest Unstable Version](https://poser.pugx.org/eveseat/seat/v/unstable)](https://packagist.org/packages/eveseat/seat)
+[![License](https://poser.pugx.org/eveseat/seat/license)](https://packagist.org/packages/eveseat/seat)
+[![Documentation Status](https://readthedocs.org/projects/seat-docs/badge/?version=latest)](http://seat-docs.readthedocs.org/en/latest/?badge=latest)
---
+++ src/AppBundle/Command/PopulateDbCommand.php
@@ -0,0 +1,31 @@
+<?php
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PopulateDbCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('db:populate')
+            ->setDescription('Populate Database with default settings')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getContainer()->get('helper');
+        $helper->generateDefaultSettings();
+    }
+}
