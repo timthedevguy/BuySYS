56ac093abafbc34d5ed356b64bdb7126ecc400bf
Tim Davis <binary.god@gmail.com>
2016-06-16 20:53:54 -0400

    Added guest buyback

---
+++ src/AppBundle/Form/BuyBackHiddenForm.php
@@ -0,0 +1,31 @@
+<?php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+class BuyBackHiddenForm extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('items', HiddenType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Model\BuyBackModel',
+        ));
+    }
+}
