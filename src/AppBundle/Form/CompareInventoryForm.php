cf76be70044fe29ebb286745e4f42e34b913af88
Tim Davis <binary.god@gmail.com>
2016-09-03 11:05:01 -0400

    Removed junk from Compare scree, got rid of Label

--- src/AppBundle/Form/CompareInventoryForm.php
+++ src/AppBundle/Form/CompareInventoryForm.php
@@ -11,7 +11,7 @@ class CompareInventoryForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('items', TextareaType::class, array('attr' => array('placeholder' => 'Paste items from game here...', 'rows' => '10')))
+            ->add('items', TextareaType::class, array('label' => false, 'attr' => array('placeholder' => 'Paste items from game here...', 'rows' => '10')))
             ->add('submit', SubmitType::class, array('label' => 'Compare', 'attr' => array('class' => 'btn btn-primary pull-right')))
         ;
     }
